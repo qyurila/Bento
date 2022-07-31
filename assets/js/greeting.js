6589d2d66bdeb1d4e85e07813df8dc1f27f7c5ec
QriLa <rea9lizer@gmail.com>
2022-07-31 19:46:40 +0900

    🐛 Fix wrong time scope for greeting messages

--- assets/js/greeting.js
+++ assets/js/greeting.js
@@ -14,7 +14,7 @@ const gree4 = `${CONFIG.greetingEvening}\xa0`;
 
 if (hour >= 23 || hour < 5) {
 	document.getElementById('greetings').innerText = gree1 + name;
-} else if (hour >= 6 && hour < 12) {
+} else if (hour >= 5 && hour < 12) {
 	document.getElementById('greetings').innerText = gree2 + name;
 } else if (hour >= 12 && hour < 17) {
 	document.getElementById('greetings').innerText = gree3 + name;
